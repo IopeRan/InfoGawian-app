d5f15413fc27326a55efa7f00fccfd068edaf123
unknown <chilliope48@gmail.com>
2024-03-01 17:49:56 +0800

    responsive create post & update post

--- resources/views/partials/admin/posts/update.blade.php
+++ resources/views/partials/admin/posts/update.blade.php
@@ -1,17 +1,17 @@
-<div id="createPost" class="hidden justify-center items-center w-full fixed top-6">
+<div id="editPost" class="hidden justify-center items-center w-full fixed top-6">
     <div class="bg-white w-full lg:w-1/3 h-max rounded-3xl mx-6 flex flex-col overflow-hidden">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#14b8a6" fill-opacity="1" d="M0,128L60,149.3C120,171,240,213,360,218.7C480,224,600,192,720,186.7C840,181,960,203,1080,213.3C1200,224,1320,224,1380,224L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
         <div class="px-6 pt-6">
             <div class="flex flex-col">
-                <h5 class="text-slate-500 font-medium text-lg lg:text-xl">Create a new Post</h5>
+                <h5 class="text-slate-500 font-medium text-lg lg:text-xl">Edit Post</h5>
                 <small class="text-slate-400 lg:text-md">17/08/2024</small>
             </div>
             <hr class="mt-3">
             <form action="" class="w-full flex flex-col gap-4 mt-4">
-                <textarea class="border w-full rounded-xl outline-teal-500 px-3 py-2 shadow-md" placeholder="Post"></textarea>
+                <textarea class="border w-full rounded-xl outline-yellow-500 px-3 py-2 shadow-md" placeholder="Post"></textarea>
                 <div class="flex justify-between">
                     <button type="button" onclick="cancelCreatePost()" class="px-6 py-2 font-medium border-red-500 border text-red-500 rounded-xl hover:bg-red-500 hover:duration-150 hover:text-white">Cancel</button>
-                    <button type="submit" class="px-6 py-2 text-white font-medium bg-teal-500 rounded-xl hover:bg-teal-400 hover:duration-150">Create</button>
+                    <button type="submit" class="px-6 py-2 text-white font-medium bg-yellow-500 rounded-xl hover:bg-yellow-400 hover:duration-150">Save Change</button>
                 </div>
             </form>
         </div>
